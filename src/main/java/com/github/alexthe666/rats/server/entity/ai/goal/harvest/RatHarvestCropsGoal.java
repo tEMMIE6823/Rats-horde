45bc8f6ae30598c7d0d36d77078e722ba2703f37
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-11-14 18:32:34 -0700

    fix #602, fix #610

--- src/main/java/com/github/alexthe666/rats/server/entity/ai/goal/harvest/RatHarvestCropsGoal.java
+++ src/main/java/com/github/alexthe666/rats/server/entity/ai/goal/harvest/RatHarvestCropsGoal.java
@@ -6,13 +6,16 @@ import com.github.alexthe666.rats.server.entity.rat.TamedRat;
 import com.github.alexthe666.rats.server.misc.RatUpgradeUtils;
 import com.github.alexthe666.rats.server.misc.RatUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.goal.Goal;
-import net.minecraft.world.level.block.AttachedStemBlock;
-import net.minecraft.world.level.block.BonemealableBlock;
-import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.StemBlock;
+import net.minecraft.world.entity.animal.Fox;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.ArrayList;
@@ -43,13 +46,8 @@ public class RatHarvestCropsGoal extends BaseRatHarvestGoal {
 		int RADIUS = this.rat.getRadius();
 		for (BlockPos pos : BlockPos.betweenClosedStream(this.rat.getSearchCenter().offset(-RADIUS, -RADIUS, -RADIUS), this.rat.getSearchCenter().offset(RADIUS, RADIUS, RADIUS)).map(BlockPos::immutable).toList()) {
 			BlockState state = this.rat.level().getBlockState(pos);
-			if (state.is(BlockTags.CROPS)) {
-				if (state.getBlock() instanceof CropBlock crop && !crop.isMaxAge(state)) continue;
-				if (!(state.getBlock() instanceof StemBlock) && !(state.getBlock() instanceof AttachedStemBlock)) {
-					if (RatUtils.canRatBreakBlock(this.rat.level(), pos, this.rat)) {
-						allBlocks.add(pos);
-					}
-				}
+			if (this.canHarvestBlock(this.rat.level(), pos, state)) {
+				allBlocks.add(pos);
 			}
 		}
 		if (!allBlocks.isEmpty()) {
@@ -68,14 +66,15 @@ public class RatHarvestCropsGoal extends BaseRatHarvestGoal {
 		if (this.getTargetBlock() != null) {
 			BlockState block = this.rat.level().getBlockState(this.getTargetBlock());
 			this.rat.getNavigation().moveTo(this.getTargetBlock().getX() + 0.5D, this.getTargetBlock().getY(), this.getTargetBlock().getZ() + 0.5D, 1.25D);
-			if (block.is(BlockTags.CROPS)) {
-				if (block.getBlock() instanceof CropBlock crop && !crop.isMaxAge(block)) {
-					this.setTargetBlock(null);
-					this.stop();
-					return;
-				}
-				double distance = this.rat.getRatDistanceCenterSq(this.getTargetBlock().getX(), this.getTargetBlock().getY(), this.getTargetBlock().getZ());
-				if (distance < this.rat.getRatHarvestDistance(0.0D)) {
+			double distance = this.rat.getRatDistanceCenterSq(this.getTargetBlock().getX(), this.getTargetBlock().getY(), this.getTargetBlock().getZ());
+			if (distance < this.rat.getRatHarvestDistance(0.0D)) {
+				if (block.is(BlockTags.CROPS) || block.is(Blocks.NETHER_WART)) {
+					if (block.getBlock() instanceof CropBlock crop && !crop.isMaxAge(block)) {
+						this.setTargetBlock(null);
+						this.stop();
+						return;
+					}
+
 					this.rat.level().destroyBlock(this.getTargetBlock(), true);
 					if ((!RatConfig.ratsBreakBlockOnHarvest || RatUpgradeUtils.hasUpgrade(this.rat, RatsItemRegistry.RAT_UPGRADE_REPLANTER.get()))) {
 						if (block.getBlock() instanceof BonemealableBlock) {
@@ -83,8 +82,49 @@ public class RatHarvestCropsGoal extends BaseRatHarvestGoal {
 						}
 					}
 					this.stop();
+				} else if (block.getBlock() instanceof SugarCaneBlock) {
+					this.rat.level().destroyBlock(this.getTargetBlock(), true);
+					this.stop();
+				} else if (block.is(Blocks.SWEET_BERRY_BUSH)) {
+					//[VanillaCopy] of Fox.FoxEatBerriesGoal
+					int i = block.getValue(SweetBerryBushBlock.AGE);
+					int j = 1 + this.rat.level().random.nextInt(2) + (i == 3 ? 1 : 0);
+					ItemStack itemstack = this.rat.getItemBySlot(EquipmentSlot.MAINHAND);
+					if (itemstack.isEmpty()) {
+						this.rat.setItemSlot(EquipmentSlot.MAINHAND, new ItemStack(Items.SWEET_BERRIES));
+						--j;
+					}
+
+					if (j > 0) {
+						Block.popResource(this.rat.level(), this.getTargetBlock(), new ItemStack(Items.SWEET_BERRIES, j));
+					}
+
+					this.rat.playSound(SoundEvents.SWEET_BERRY_BUSH_PICK_BERRIES, 1.0F, 1.0F);
+					this.rat.level().setBlock(this.getTargetBlock(), block.setValue(SweetBerryBushBlock.AGE, 1), 2);
+					this.stop();
+				} else if (CaveVines.hasGlowBerries(block)) {
+					CaveVines.use(this.rat, block, this.rat.level(), this.getTargetBlock());
+					this.stop();
 				}
 			}
 		}
+	}
+
+	private boolean canHarvestBlock(Level level, BlockPos pos, BlockState state) {
+		if (!RatUtils.canRatBreakBlock(this.rat.level(), pos, this.rat)) {
+			return false;
+		}
+		if (state.is(BlockTags.CROPS)) {
+			if (state.getBlock() instanceof CropBlock crop && !crop.isMaxAge(state)) return false;
+			if (!(state.getBlock() instanceof StemBlock) && !(state.getBlock() instanceof AttachedStemBlock)) {
+				return true;
+			}
+		} else if (state.getBlock() instanceof SugarCaneBlock) {
+			return level.getBlockState(pos.below()).getBlock() instanceof SugarCaneBlock;
+		} else if (state.getBlock() instanceof NetherWartBlock && state.getValue(NetherWartBlock.AGE) >= 3) {
+			return true;
+		}
+
+		return (state.is(Blocks.SWEET_BERRY_BUSH) && state.getValue(SweetBerryBushBlock.AGE) > 1) || CaveVines.hasGlowBerries(state);
 	}
 }
